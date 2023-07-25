95d829033c2b3e188fe86ab350eb54164ba2548b
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-07-24 21:18:50 -0300

    24/07

--- kilo_marmita/src/App.js
+++ kilo_marmita/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from "./Componentes/Header/Header"
 import Test from './Componentes/Carousel/Carousel';
+import InformationText from './Componentes/InformationsText/InformationText';
 
 function App() {
   return (
@@ -9,11 +10,11 @@ function App() {
 
       <Header links={[["", "Inicio"], ["", "Serviços"], ["", "Produtos"], ["", "Cardápio"], ["", "Faça Seu Pedido"]]}></Header>
       
-      <div className='container mt-5 mb-5 d-flex justify-content-start'>
+      <div className='container mt-4 mb-4 d-flex justify-content-start'>
 
         <Test></Test>
-
         
+        <InformationText></InformationText>
 
       </div>
 
--- kilo_marmita/src/Componentes/InformationsText/InformationText.js
+++ kilo_marmita/src/Componentes/InformationsText/InformationText.js
@@ -1,5 +1,27 @@
 import React from "react";
+import "./InformationText.css"
 
 
+function InformationText() {
 
+    return (
+        <div className="container d-flex">
 
+            <div className="v-row"></div>
+
+            <div>
+                <h1>A Melhor Marmita de Campina Grande</h1>
+                <br></br>
+                <p>
+                    Temos por principal objetivo fornecer refeições de qualidade aos nossos clientes,
+                    garantindo-lhes satisfação ao saborear os diversos pratos disponíveis em nosso cardápio.
+                    Buscamos sempre a excelência dos nossos produtos e a satisfação dos nossos clientes.
+                </p>
+                <h3>CONTATO: 3321-8786 | 3055-0246</h3>
+            </div>
+        </div>
+    )
+
+}
+
+export default InformationText;
