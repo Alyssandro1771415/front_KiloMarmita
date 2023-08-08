d7e3b320a9135d5a430fc9334730a6206875ec66
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-08-08 20:42:00 -0300

    08/08

--- kilo_marmita/src/Componentes/CartLateralButton/CartLateralButton.js
+++ kilo_marmita/src/Componentes/CartLateralButton/CartLateralButton.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CartLateralButton.css"
+import image from "../images/feijoada.png"
 
 function CartLateralButton() {
 
+    const [lister, setLister] = useState(false);
+
+    useEffect(() => {
+        // O estado 'lister' controla a visibilidade da lista
+    }, [lister]);
+
+    const toggleList = () => {
+        setLister(prevLister => !prevLister); // Inverte o valor do estado 'lister'
+    };
+
     return (
-        <button className="cart-button">
-            <i class="bi bi-arrow-bar-down arrow-down"></i>
-            <i class="bi bi-cart-check-fill"> Cart</i>
-        </button>
+        <div className="cart-button d-flex flex-column">
+            <div className="list-items-button" onClick={toggleList}>
+                <i className="bi bi-arrow-bar-down arrow-down"></i>
+                <span className="bi bi-cart-check-fill">Cart</span>
+            </div>
+            <div className={`list-items ${lister ? "" : "d-none"}`}>
+                <hr />
+                <ul>
+                    <li>
+                        <div className="d-flex justify-content-around">
+                            <img src={image}></img>
+                            <div>
+                                <p>Feijoada</p>
+                                <p1>15.50</p1>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="d-flex justify-content-around">
+                            <img src={image}></img>
+                            <div>
+                                <p>Feijoada</p>
+                                <p1>15.50</p1>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="d-flex justify-content-around">
+                            <img src={image}></img>
+                            <div>
+                                <p>Feijoada</p>
+                                <p1>15.50</p1>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <h5>Total: R$</h5>
+            </div>
+        </div>
     )
 }
 
